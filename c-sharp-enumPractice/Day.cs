1d2d603156e7616e67f8dd9b33257e8bbad190d3
omranali2111 <aljabri1234@hotmail.com>
2023-07-06 09:07:15 +0400

    task1:using enum represent the days of   the week

---
+++ c-sharp-enumPractice/Day.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_enumPractice
+{
+    /*
+  1. Create an enum called DaysOfWeek to represent the days of 
+  the week(Sunday, Monday, Tuesday, etc.). Write a program that 
+  prompts the user to enter a day of the week and displays a message
+  based on the entered value.Use the switch statement to handle different cases.
+  */
+    internal class Day
+    {
+
+        
+
+        public Day() { }
+       
+        
+    }
+
+    enum DaysOfWeek
+    {
+        SUNDAY,
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+
+
+    }
+}
---
+++ c-sharp-enumPractice/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Buffers.Text;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata;
+using System;
+using c_sharp_enumPractice;
+
+internal class Program
+{
+
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("enter a day:");
+        string day = Console.ReadLine();
+        DayOfWeek d = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), day);//convert from enum type to string 
+
+        switch (d)
+        {
+            case DayOfWeek.Sunday:
+                Console.WriteLine("the first day of the week");
+                break;
+            case DayOfWeek.Monday:
+                Console.WriteLine("the second day of the week");
+                break;
+            case DayOfWeek.Tuesday:
+                Console.WriteLine("the third day of the week");
+                break;
+            case DayOfWeek.Wednesday:
+                Console.WriteLine("the fourth day of the week");
+                break;
+            case DayOfWeek.Thursday:
+                Console.WriteLine("the fifth day of the week");
+                break;
+            case DayOfWeek.Friday:
+                Console.WriteLine("the sixth day of the week");
+                break;
+            case DayOfWeek.Saturday:
+                Console.WriteLine("the seventh day of the week");
+                break;
+
+
+        }
+
+
+    }
+}
+       
+
+        
+       
+       
+
