1e64b91500bdbdf3f4be76dc1eba1b326060c19c
omranali2111 <aljabri1234@hotmail.com>
2023-07-06 09:27:37 +0400

    task2:enum called ClothingSize to represent different clothing sizes

---
+++ c-sharp-enumPractice/Clothing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+namespace c_sharp_enumPractice
+{
+    /*
+    2. Create an enum called ClothingSize to represent different clothing
+    sizes(Small, Medium, Large, etc.). Write a program that prompts the user
+    to enter their clothing size and displays a message based on the entered 
+    value.Use an if-else statement to handle different cases.
+    */
+    internal class Clothing
+    {
+    }
+
+    enum ClothingSize
+    {
+        SMALL,
+        MEDIUM,
+        LARGE,
+        EXTRALARGE,
+
+
+    }
+
+
+}
